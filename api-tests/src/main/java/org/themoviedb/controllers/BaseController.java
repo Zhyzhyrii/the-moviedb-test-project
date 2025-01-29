7004ebb13fe273d97b85fa2682c3ddf924516927
izhyzhyrii <izhizhirii@gmail.com>
2025-01-29 22:32:38 +0200

    Change TODO in BaseController

--- api-tests/src/main/java/org/themoviedb/controllers/BaseController.java
+++ api-tests/src/main/java/org/themoviedb/controllers/BaseController.java
@@ -9,7 +9,7 @@ import io.restassured.specification.RequestSpecification;
 public class BaseController {
 
     protected RequestSpecification baseClient() {
-//        TODO: implement token provider via api
+//        TODO: to be stored on safe place
         var authToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMGM5ZDBmN2QyZTg5YjA5MjYzYmFmYWFmOGM2OWE2YSIsIm5iZiI6MTU2MzQ2Mzg0My4xOTEsInN1YiI6IjVkMzA5MGEzY2FhYjZkMTY0MGExNzZjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dAAGUsz0vwhS8Th1gW2PowRoA4jjkK5LXiRkOHYi96Y";
         return RestAssured.given()
                 .baseUri("https://api.themoviedb.org/3")
