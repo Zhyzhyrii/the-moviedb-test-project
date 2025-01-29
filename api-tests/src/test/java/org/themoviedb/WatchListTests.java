f41a699be413407313f11c2ed32d7674344352fc
izhyzhyrii <izhizhirii@gmail.com>
2025-01-29 22:42:04 +0200

    Refactor WatchListTests

--- api-tests/src/test/java/org/themoviedb/WatchListTests.java
+++ api-tests/src/test/java/org/themoviedb/WatchListTests.java
@@ -1,26 +1,35 @@
 package org.themoviedb;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.themoviedb.facades.AccountControllerFacade;
 import org.themoviedb.facades.MovieListsControllerFacade;
+import org.themoviedb.models.Movie;
 
 public class WatchListTests {
 
+    private final MovieListsControllerFacade movieListsControllerFacade = new MovieListsControllerFacade();
+    private final AccountControllerFacade accountControllerFacade = new AccountControllerFacade();
+
+    private Movie randomMovie;
+
+    @BeforeMethod
+    public void setUp() {
+        randomMovie = movieListsControllerFacade.getRandomTopRatedMovie();
+    }
+
     @Test
     public void addRandomMovieFromTopRatedToWatchListAndVerifyThatItWasAdded() {
-//        PRECONDITION
-        var movieListsControllerFacade = new MovieListsControllerFacade();
-        var randomMovie = movieListsControllerFacade.getRandomTopRatedMovie();
-
-//        TEST
-        var accountControllerFacade = new AccountControllerFacade();
         accountControllerFacade.addMovieToWatchlist(randomMovie);
 
         var actualMovie = accountControllerFacade.getFirstWatchlistMovie();
-        Assert.assertEquals(actualMovie, randomMovie, "Wrong movie was added to watchlist");//TODO
+        Assert.assertEquals(actualMovie, randomMovie, "Wrong movie was added to watchlist");
+    }
 
-//        POST condition
+    @AfterMethod
+    public void tearDown() {
         accountControllerFacade.removeMovieFromWatchlist(randomMovie);
     }
 }
