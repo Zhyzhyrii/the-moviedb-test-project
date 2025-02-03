4b2f329d568c1a0f5d2bb07af49131f437974aa0
izhyzhyrii <izhizhirii@gmail.com>
2025-02-03 22:34:42 +0200

    Add PostResponseDto

--- api-tests/src/main/java/org/themoviedb/models/PostResponseDto.java
+++ api-tests/src/main/java/org/themoviedb/models/PostResponseDto.java
@@ -1,10 +1,7 @@
 package org.themoviedb.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
-
-import java.util.List;
 
 @Value
 public class PostResponseDto {
