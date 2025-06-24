fdc51bc4d3afe76fe888416d7e80ebcc7631e68f
Zhyzhyrii <Zhyzhyrii@users.noreply.github.com>
2025-06-24 11:40:36 +0000

    deploy: 54f61276ad91cd1f0d09d105ccf838b6b5fd7c69

--- api-tests/src/main/java/org/themoviedb/data/BodyPaths.java
+++
@@ -1,8 +0,0 @@
-package org.themoviedb.data;
-
-public class BodyPaths {
-
-    public static final String SUCCESS = "success";
-    public static final String STATUS_CODE = "status_code";
-    public static final String STATUS_MESSAGE = "status_message";
-}
--- api-tests/src/test/java/org/themoviedb/BaseTest.java
+++
@@ -1,9 +0,0 @@
-package org.themoviedb;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-
-@SuppressWarnings("PMD.TestClassWithoutTestCases")
-@SpringBootTest(classes = Application.class)
-public class BaseTest extends AbstractTestNGSpringContextTests {
-}
