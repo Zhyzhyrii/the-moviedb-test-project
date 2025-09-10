481f71e667a241f7410fa3947f67baf3c2a1e029
Zhyzhyrii <izhizhirii@gmail.com>
2025-09-10 23:43:48 +0300

    Refactoring: add token provider (#33)

--- api-tests/src/main/java/org/themoviedb/PathQueryParameters.java
+++ api-tests/src/main/java/org/themoviedb/PathQueryParameters.java
@@ -1,6 +1,6 @@
 package org.themoviedb;
 
-public class PathQueryParameters {
+public class PathQueryParameters {//todo to controllers, with urls
 
     public static final String LIST_ID = "list_id";
     public static final String ACCOUNT_ID = "accountId";
